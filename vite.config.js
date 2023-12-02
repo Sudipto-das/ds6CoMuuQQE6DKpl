469f57d7d0283bc99342240e480ce0c8bbd55a23
Sudipto das <sudiptodas877@gmail.com>
2023-12-02 18:03:42 +0530

    final try deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({
+export default defineConfig({
  
-   
-    build: {
-      outDir: 'build'
-    }
+  base:'/ds6comuuqqe6dkpl/',
+  plugins:[react()]
   
 })
